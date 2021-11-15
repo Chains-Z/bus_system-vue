f06e0e0f2df9a16759d8ad83041d16c6b7cf9e57
Chains-Z <57571141+Chains-Z@users.noreply.github.com>
2021-11-15 21:46:18 +0800

    chore:在原型上绑定$log
    在main.js里将console.log绑定为全局的$log,以便在模板里使用

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import axios from "axios"
 import VueAxios from "vue-axios"
 
 const app = createApp(App)
-
+app.config.globalProperties.$log = console.log
 app.use(VueAxios,axios).use(router).mount('#app')
