5fb6dc43d07a8ae201cad81f391f36661bf94f41
Chains-Z <57571141+Chains-Z@users.noreply.github.com>
2021-11-17 03:01:05 +0800

    chore:引入qs用于处理url

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios"
 import VueAxios from "vue-axios"
+import qs from 'qs'
 
 const app = createApp(App)
 app.config.globalProperties.$log = console.log
+app.config.globalProperties.qs = qs
 app.use(VueAxios,axios).use(router).mount('#app')
