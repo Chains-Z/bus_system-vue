59f3db705f43257759af1e679ada83e9147ef146
Chains-Z <57571141+Chains-Z@users.noreply.github.com>
2021-11-20 23:25:00 +0800

    factor:最短路径按照长度排序

--- src/components/station/ShortestPathQuery.vue
+++ src/components/station/ShortestPathQuery.vue
@@ -65,11 +65,11 @@ export default {
     processData() {
       this.paths = this.data.map(path => {
         let res = {}
-        res.startId = path.stationId[0]
-        res.endId = path.stationId[path.stationId.length - 1]
+        res.startId = path['stationId'][0]
+        res.endId = path['stationId'][path['stationId'].length - 1]
         let p = ''
         for (let i = 0; i < path.lineName.length; i++) {
-          p += `${path.stationName[i]}--${path.lineName[i]}-->`
+          p += `${path['stationName'][i]}--${path['lineName'][i]}-->`
         }
         p += path.stationName[path.stationName.length - 1]
         res.path = p
@@ -84,10 +84,9 @@ export default {
         if(res.data.isok) {
           let data = res.data.data
           console.log(data)
-          if (data instanceof Array)
-            this.data = data
-          else
-            this.data = [data]
+          if(!this.idQuery)
+            data.sort((a,b)=>a['stationId'].length - b['stationId'].length)
+          this.data = dat
           this.processData()
           ElMessage.success("查询成功！")
         }
