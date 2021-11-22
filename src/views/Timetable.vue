7bf0d2894efd29371dc525241ea98c6c2712219a
Chains-Z <57571141+Chains-Z@users.noreply.github.com>
2021-11-22 22:05:07 +0800

    factor:完成班次查询的按站点展示

--- src/views/Timetable.vue
+++ src/views/Timetable.vue
@@ -10,21 +10,21 @@
         <el-form-item>
           <el-input
               v-model="searchParams"
-              placeholder="请输入线路名称"
+              placeholder="请输入线路名称(包括上下行)"
               @keyup.enter="getData">
           </el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary"  @click="getData">搜索</el-button>
+          <el-button type="primary" @click="getData">搜索</el-button>
         </el-form-item>
       </el-form>
     </el-col>
   </el-row>
   <el-row>
     <el-col :span="14" :offset="5">
-      <el-table :data="data" border :stripe="true">
-        <el-table-column type="index" :index="1" label="第几班" width="100"/>
-        <el-table-column prop="timetable" label="时间表"/>
+      <el-table :data="data" border :stripe="true" max-height="600">
+        <el-table-column fixed type="index" :index="1" label="第几班" width="100"/>
+        <el-table-column v-for="station in stations" :prop="station.name" :label="station.name"/>
       </el-table>
     </el-col>
   </el-row>
@@ -38,32 +38,36 @@ export default {
   name: "Timetable",
   data() {
     return {
-      searchParams : '',
-      data : undefined
+      searchParams: '',
+      data: undefined,
+      stations : [{name:'时间表'}],
     }
   },
-  methods:{
-    processData(){
-      this.data = this.data.map(timetable=>{
-        timetable = timetable.map(time => ' '+time.slice(0,-3))
-        let o = {}
-        o.timetable = timetable
-        return o
-      })
-    },
-    getData(){
-      let url = `/line/get_timetable_of/${this.searchParams}`
-      this.axios.get(url).then(res=>{
-        if(res.data.isok) {
-          let data = res.data.data
+  methods: {
+    getData() {
+      let url1 = `/line/get_timetable_of/${this.searchParams}`
+      let url2 = `/line/stations/${this.searchParams}`
+      Promise.all([this.axios.get(url1),this.axios.get(url2)]).then(res =>{
+        if (res[0].data.isok && res[1].data.isok) {
+          let timetable = res[0].data.data
+          let stations = res[1].data.data
+          this.stations = stations
+          let data = []
+          // for (const station of stations) {
+          //   o[station.name] = []
+          // }
+          for (let i = 0; i < timetable.length; i++) {
+            data[i] = {}
+            for (let j = 0; j < timetable[0].length; j++) {
+              data[i][stations[j].name] = timetable[i][j]
+            }
+          }
           console.log(data)
           this.data = data
-          data && this.processData()
           ElMessage.success("查询成功！")
-        }
-        else
+        } else
           throw new Error(res.data.message)
-      }).catch(error=>ElMessage.error(error.toString()))
+      }).catch(error => ElMessage.error(error.toString()))
     }
   }
 }
